659d46913603219ba460738cc35b33838d3beaf5
voiduniversity <davidchismihai06@gmail.com>
2026-04-21 10:02:48 +0100

    F: added .gitignore & modified easyCode

--- easyCode.py
+++ easyCode.py
@@ -3,7 +3,7 @@ import tkinter as tk
 from PIL import Image
 import os
 
-python = "pythonCodingFile.txt"
+python = "pythonEasyFile.txt"
 score = "rightandwrong.txt"
 pythonCodeLines = []
 pyhtonExample = ""
